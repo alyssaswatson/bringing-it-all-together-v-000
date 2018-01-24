7ed9043613bacaa721273f9c3d1898e80389c159
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-01-24 19:26:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -67,7 +67,16 @@ class Dog
   end
 
   def self.find_by_id(id)
-
+    sql= <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id 
+      IS ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.find_or_create_by(row)
